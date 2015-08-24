469a03106110e70fe39ef09a8e71cceed9c327b3
Adam Burmister <adam.burmister@gmail.com>
2015-08-23 22:29:35 -0700

    layout switch toggle labels

--- project/src/js/views/Settings.js
+++ project/src/js/views/Settings.js
@@ -29,69 +29,83 @@ class Settings extends React.Component {
 
           <fieldset>
             <legend>Notifications</legend>
-            <div className="toggle-switch">
-              <input type="checkbox" id="check1" />
-              <label htmlFor="check1"></label>
-            </div>
-            <label htmlFor="check1">email me when my posts are marked as favorites</label>
 
-            <div className="toggle-switch">
-              <input type="checkbox" id="check2" />
-              <label htmlFor="check2"></label>
+            <div className="radio">
+              <div className="toggle-switch">
+                <input type="checkbox" id="check1" />
+                <label htmlFor="check1"></label>
+              </div>
+              <label htmlFor="check1">email me when my posts are marked as favorites</label>
             </div>
-            <label htmlFor="check2">email me when I'm mentioned</label>
 
-            <div className="toggle-switch">
-              <input type="checkbox" id="check3" />
-              <label htmlFor="check3"></label>
+            <div className="radio">
+              <div className="toggle-switch">
+                <input type="checkbox" id="check2" />
+                <label htmlFor="check2"></label>
+              </div>
+              <label htmlFor="check2">email me when I'm mentioned</label>
             </div>
-            <label htmlFor="check3">email me when I get a reply</label>
 
-            <div className="toggle-switch">
-              <input type="checkbox" id="check4" />
-              <label htmlFor="check4"></label>
+            <div className="radio">
+              <div className="toggle-switch">
+                <input type="checkbox" id="check3" />
+                <label htmlFor="check3"></label>
+              </div>
+              <label htmlFor="check3">email me when I get a reply</label>
             </div>
-            <label htmlFor="check4">email me when someone follows me</label>
 
+            <div className="radio">
+              <div className="toggle-switch">
+                <input type="checkbox" id="check4" />
+                <label htmlFor="check4"></label>
+              </div>
+              <label htmlFor="check4">email me when someone follows me</label>
+            </div>
           </fieldset>
 
           <fieldset>
             <legend>Privacy</legend>
             <div className="radio-group">
-              <p>
+              <div>
                 <label className="radio">
                   <input type="radio" name="tagging" /> allow anyone to tag me
                 </label>
-              </p>
-              <p>
+              </div>
+              <div>
                 <label className="radio">
                   <input type="radio" name="tagging" /> only allow people I follow to tag me
                 </label>
-              </p>
-              <p>
+              </div>
+              <div>
                 <label className="radio">
                   <input type="radio" name="tagging" /> don't allow anyone to tag me
                 </label>
-              </p>
+              </div>
             </div>
 
-            <div className="toggle-switch">
-              <input type="checkbox" id="check5" />
-              <label htmlFor="check5"></label>
+            <div className="radio">
+              <div className="toggle-switch">
+                <input type="checkbox" id="check5" />
+                <label htmlFor="check5"></label>
+              </div>
+              <label htmlFor="check5">add a location to my posts</label>
             </div>
-            <label htmlFor="check5">add a location to my posts</label>
 
-            <div className="toggle-switch">
-              <input type="checkbox" id="check6" />
-              <label htmlFor="check6"></label>
+            <div className="radio">
+              <div className="toggle-switch">
+                <input type="checkbox" id="check6" />
+                <label htmlFor="check6"></label>
+              </div>
+              <label htmlFor="check6">let others find me by email address</label>
             </div>
-            <label htmlFor="check6">let others find me by email address</label>
 
-            <div className="toggle-switch">
-              <input type="checkbox" id="check7" />
-              <label htmlFor="check7"></label>
+            <div className="radio">
+              <div className="toggle-switch">
+                <input type="checkbox" id="check7" />
+                <label htmlFor="check7"></label>
+              </div>
+              <label htmlFor="check7">tailor adds by my information</label>
             </div>
-            <label htmlFor="check7">tailor adds by my information</label>
 
           </fieldset>
 
