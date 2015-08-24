98965499a3afd3bab18d03ef3cf1fda3cdc54350
Adam Burmister <adam.burmister@gmail.com>
2015-08-23 21:40:08 -0700

    Sketch for the settings page

--- project/src/js/views/Settings.js
+++ project/src/js/views/Settings.js
@@ -11,7 +11,87 @@ class Settings extends React.Component {
   render() {
     return (
       <div className="container settings">
-        Settings go here
+        <form className="settings--form">
+          <h1>Settings</h1>
+
+          <fieldset>
+            <legend>Account</legend>
+            <div class="settings--avatar">
+
+            </div>
+            <div class="settings--login">
+              Name
+              Email
+              Password
+              Change
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <legend>Notifications</legend>
+            <p>
+              <label class="toggle-switch">
+                <input type="checkbox" /> email me when my posts are marked as favorites
+              </label>
+            </p>
+            <p>
+              <label class="toggle-switch">
+                <input type="checkbox" /> email me when I'm mentioned
+              </label>
+            </p>
+            <p>
+              <label class="toggle-switch">
+                <input type="checkbox" /> email me when I get a reply
+              </label>
+            </p>
+            <p>
+              <label class="toggle-switch">
+                <input type="checkbox" /> email me when someone follows me
+              </label>
+            </p>
+          </fieldset>
+
+          <fieldset>
+            <legend>Privacy</legend>
+            <div class="radio-group">
+              <p>
+                <label class="radio">
+                  <input type="radio" name="tagging" /> allow anyone to tag me
+                </label>
+              </p>
+              <p>
+                <label class="radio">
+                  <input type="radio" name="tagging" /> only allow people I follow to tag me
+                </label>
+              </p>
+              <p>
+                <label class="radio">
+                  <input type="radio" name="tagging" /> don't allow anyone to tag me
+                </label>
+              </p>
+            </div>
+
+            <p>
+              <label class="toggle-switch">
+                <input type="checkbox" /> add a location to my posts
+              </label>
+            </p>
+            <p>
+              <label class="toggle-switch">
+                <input type="checkbox" /> let others find me by email address
+              </label>
+            </p>
+            <p>
+              <label class="toggle-switch">
+                <input type="checkbox" /> tailor adds by my information
+              </label>
+            </p>
+          </fieldset>
+
+          <div className="button-footer">
+            <button type="button">Save Changes</button>
+          </div>
+        </form>
       </div>
     );
   }
